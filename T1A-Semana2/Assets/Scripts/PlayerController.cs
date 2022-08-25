216f29631e005b4d2813cffe607e07fd2fd6c327
Nicky Aguilar <91038547+NicoldAguilar@users.noreply.github.com>
2022-08-25 08:16:22 -0500

    SinSalto

--- T1A-Semana2/Assets/Scripts/PlayerController.cs
+++ T1A-Semana2/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float velocity = 5, jumpForce = 3;
+
+    Rigidbody2D gravedad;
+    SpriteRenderer renderi;
+    Animator animador;
+
+    bool saltos;
+
+    const int ANIMATION_QUIETO = 0;
+    const int ANIMATION_CAMINAR = 1;
+    const int ANIMATION_CORRER = 2;
+    const int ANIMATION_SALTAR = 3;
+    const int ANIMATION_ATACAR = 4;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Ha iniciado el juego");
+        gravedad = GetComponent<Rigidbody2D>();
+        renderi = GetComponent<SpriteRenderer>();
+        animador = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gravedad.velocity = new Vector2(0, gravedad.velocity.y);
+        ChangeAnimation(ANIMATION_QUIETO);
+
+        //rb.velocity.x:
+        //rb.velocity.y:
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gravedad.velocity = new Vector2(velocity, gravedad.velocity.y);
+            renderi.flipX = false;
+            ChangeAnimation(ANIMATION_CAMINAR);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gravedad.velocity = new Vector2(-velocity, gravedad.velocity.y);
+            renderi.flipX = true;
+            ChangeAnimation(ANIMATION_CAMINAR);
+        }
+        if(Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.X))
+        {
+            gravedad.velocity = new Vector2(velocity, gravedad.velocity.y);
+            renderi.flipX = false;
+            ChangeAnimation(ANIMATION_CORRER);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.X))
+        {
+            gravedad.velocity = new Vector2(-velocity, gravedad.velocity.y);
+            renderi.flipX = true;
+            ChangeAnimation(ANIMATION_CORRER);
+        }
+        //Atacar 
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.Z))
+        {
+            ChangeAnimation(ANIMATION_ATACAR);
+        }
+        //Añadir fuerza para el salto
+        if (Input.GetKeyUp(KeyCode.Space) && saltos)
+        {
+            gravedad.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); //método recomendado
+            ChangeAnimation(ANIMATION_SALTAR);
+            saltos = false; //salta una vez
+            
+        }
+    }
+
+    private void ChangeAnimation(int animation)
+    {
+        animador.SetInteger("Estado", animation);
     }
 }
